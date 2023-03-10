632fd53f847a4b379a6c7e70bc9c2dd81b3de01d
EcheCostabel <exequielcostabel@gmail.com>
2023-03-10 12:13:58 -0300

    adding gif

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { GifGridItem } from './GifGridItem'
 
 export const GifGrid = ({category}) => {
+
+    const [images, setimages] = useState([])
+
+    useEffect(() => {
+        getGift()
+
+    }, [])
 
     const getGift = async () => {
         const url = 'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=76yeVqfKmzDKiuR5LLoshSQ0pYwqORR1'
@@ -17,12 +25,23 @@ export const GifGrid = ({category}) => {
         })
 
         console.log(gifs)
+        setimages(gifs)
     }
-    getGift()
 
   return (
-    <div>
+    <>
         <h3>{category}</h3>
+      <div className='card-grid'>
+       
+            {
+                images.map(img => {
+                    return <GifGridItem  key={img.id}
+                    {...img }
+                    />
+                })
+            }
+       
      </div>
+            </>
   )
 }
