ce7841ecd52662b9b8763405a7829d58873cc980
EcheCostabel <exequielcostabel@gmail.com>
2023-03-13 09:51:18 -0300

    adding css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,38 +1,21 @@
-import React from 'react'
-import { useFetchGifs } from '../hooks/useFetchGifs'
-// import { GifGridItem } from './GifGridItem'
-// import { getGifs } from '../helpers/getGifs'
+import React from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifGridItem } from "./GifGridItem";
 
-export const GifGrid = ({category}) => {
-
-    // const [images, setimages] = useState([])
-    const  {loading} = useFetchGifs();
-    console.log(loading)
-  
-
-    // useEffect(() => {
-    //     getGifs(category)
-    //     .then(imgs => setimages(imgs))
-    // }, [category])
-
+export const GifGrid = ({ category }) => {
+  const { data, loading } = useFetchGifs(category); // este 'category' va a caer en useFetchGifs como props
 
   return (
     <>
-        <h3>{category}</h3>
-        {
-          loading ? 'Cargando...' : 'Data Cargada'
-        }
+      <h3>{category}</h3>
 
-      {/* <div className='card-grid'>
-            {
-                images.map(img => {
-                    return <GifGridItem  key={img.id}
-                    {...img }
-                    />
-                })
-            }
-       
-     </div> */}
-            </>
-  )
-}
+      {loading && <p>Loading</p>}
+
+      <div className="card-grid">
+        {data.map((img) => {
+          return <GifGridItem key={img.id} {...img} />;
+        })}
+      </div>
+    </>
+  );
+};
