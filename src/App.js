a69fc6b2c5fc7ce2dbe4947aa3c211f336d36b3e
EcheCostabel <exequielcostabel@gmail.com>
2023-03-14 08:57:33 -0300

    adding use effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 
 import './App.css';
 // import { CounterApp } from './components/01-useState/CounterApp';
-import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+import { SimpleForm } from './components/useEffect/SimpleForm';
 // import { HooksApp } from './HooksApp';
 
 function App() {
   return (
     <div className="App">
-      <CounterWithCustomHook /> 
+      <SimpleForm /> 
     </div>
   );
 }
