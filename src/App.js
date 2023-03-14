6063f221442d297b256c998019d6f2e75317dd54
EcheCostabel <exequielcostabel@gmail.com>
2023-03-14 11:44:03 -0300

    adding reducers

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 
-import './App.css';
-// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
-import { FocusScreen } from './components/03-useRef/FocusScreen';
-// import { CounterApp } from './components/01-useState/CounterApp';
-// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
-// import { SimpleForm } from './components/02-useEffect/SimpleForm';
-// import { HooksApp } from './HooksApp';
+// import './App.css';
+// // import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { FocusScreen } from './components/03-useRef/FocusScreen';
+// // import { CounterApp } from './components/01-useState/CounterApp';
+// // import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// // import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// // import { HooksApp } from './HooksApp';
+import './components/04-useReducer/intro-reducer'
 
 function App() {
   return (
     <div className="App">
-      <FocusScreen /> 
+      {/* <FocusScreen />  */}
     </div>
   );
 }
 
 export default App;
+
