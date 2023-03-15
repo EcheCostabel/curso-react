5bd2cf6a234ff3c6b38d2d65bc8346ab10e00280
EcheCostabel <exequielcostabel@gmail.com>
2023-03-15 14:40:45 -0300

    componetizando

--- src/components/04-useReducer/TodoApp.js
+++ src/components/04-useReducer/TodoApp.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer } from 'react';
 import { useForm } from '../../hooks/useForm';
 import './styles.css';
+import { TodoList } from './TodoList';
 import { todoReducer } from './todoReducer';
 
 
@@ -67,16 +68,11 @@ export const TodoApp = () => {
 
        <div className='row'>
         <div className='col-7'>
-            <ul className='list-group list-group-flush'>
-                { 
-                    todos.map( (todo, i) => (
-                    <li key={todo.id} className='list-group-item d-flex align-items-center justify-content-between'>
-                        <p  className={`${todo.done && 'complete'} `} onClick={() => handleToggle(todo.id)} >{i + 1} {todo.desc}</p>
-                        <button className='btn btn-danger' onClick={() => handleDelete(todo.id)}>Borrar</button>
-                        </li>
-                        ))
-                    }
-            </ul>
+            <TodoList  
+            todos={todos}
+            handleDelete={handleDelete}
+            handleToggle={handleToggle}
+            />
         </div> 
 
         <div className='col-5'>
